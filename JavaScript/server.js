f9d7a852228ec747ba36752f3a978abda8f7c74c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-07-14 01:50:48 +0300

    Use ws.clients

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -17,18 +17,15 @@ server.listen(8000, () => {
 
 const ws = new WebSocket.Server({ server });
 
-const clients = [];
-
 ws.on('connection', (connection, req) => {
-  clients.push(connection);
   const ip = req.socket.remoteAddress;
   console.log(`Connected ${ip}`);
   connection.on('message', message => {
     console.log('Received: ' + message);
-    for (const client of clients) {
-      if (connection !== client) {
-        client.send(message);
-      }
+    for (const client of ws.clients) {
+      if (client.readyState !== WebSocket.OPEN) continue;
+      if (client === connection) continue;
+      client.send(message);
     }
   });
   connection.on('close', () => {
